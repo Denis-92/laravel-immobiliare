b5a7a0e7af37e8fd56cd07ee987ad4b8c7c6e739
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-11-04 15:55:57 +0100

    Crazione struttura tabella houses

--- database/migrations/2022_11_04_142552_create_houses_table.php
+++ database/migrations/2022_11_04_142552_create_houses_table.php
@@ -13,8 +13,32 @@ class CreateHousesTable extends Migration
      */
     public function up()
     {
+
+/*
+dimensione
+prezzo
+descrizione
+classe energetica
+indirizzo
+piani
+proprietario
+numero di stanze
+data di costruzione
+garage
+*/
+
         Schema::create('houses', function (Blueprint $table) {
             $table->id();
+            $table->unsignedDecimal('dimensione', 6, 2);
+            $table->unsignedDecimal('prezzo', 16, 2);
+            $table->text('descrizione');
+            $table->string('classe_energetica', 2);
+            $table->string('indirizzo', 100);
+            $table->tinyInteger('piani');
+            $table->string('proprietario', 100);
+            $table->tinyInteger('numero_di_stanze');
+            $table->date('data_di_costruzione');
+            $table->boolean('garage');
             $table->timestamps();
         });
     }
