aaa8ca079fb94a3e19cfb0ca0d36c123cd5c7bf5
PietroDanieli <1pie7d@gmail.com>
2022-11-10 16:49:05 +0100

    implement show house details

--- resources/views/houses/show.blade.php
+++ resources/views/houses/show.blade.php
@@ -1,21 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-
 <body>
-
-    @foreach ($houses as $house)
-        <div>
-            <a href="{{route('houses.show', $house->id)}}">{{ $house->proprietario }} </a>
-        </div>
-    @endforeach
-
+    <h1>Details</h1>
+    <p>{{ $house->descrizione }}</p>
+    <a href="/">Back</a>
 </body>
-
 </html>
